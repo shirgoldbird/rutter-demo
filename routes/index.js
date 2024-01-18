4fcde8f894e98e297e5de29a3c51d5de2c1f3d34
Shir Goldberg <hello@shirgoldberg.com>
2024-01-17 19:41:51 -0500

    display AP account data on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 let express = require('express');
 let router = express.Router();
-let { getAccounts } = require("../rutter/Accounts.js");
+let { getAccounts, getAccount } = require("../rutter/Accounts.js");
 let { getVendors } = require("../rutter/Vendors.js");
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-    const accountData = await getAccounts();
-    const vendorData = await getVendors();
+  const accountData = await getAccounts();
+  const vendorData = await getVendors();
+  const accountsPayableAccount = await getAccount('Accounts Payable (A/P)');
 
-    res.render('index', { 
-      accounts: accountData.accounts,
-      vendors: vendorData.vendors
-    });
+  res.render('index', { 
+    accounts: accountData.accounts,
+    accountsPayableAccount: accountsPayableAccount,
+    vendors: vendorData.vendors
+  });
 });
 
 module.exports = router;
